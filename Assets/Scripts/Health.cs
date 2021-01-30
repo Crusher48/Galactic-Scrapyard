f450698ae86b84c4a417f0c611a1008a738d35e4
Anthony Carter <anthonywc117@sbcglobal.net>
2021-01-29 19:31:47 -0500

    Basic damage indicator on object

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,11 +6,17 @@ public class Health : MonoBehaviour
 {
     public float health = 100;
     public float maxHealth = 100;
+    Color baseColor;
+    private void Start()
+    {
+        baseColor = GetComponent<SpriteRenderer>().color;
+    }
     public void ChangeHealth(float netChange)
     {
         health += netChange;
         if (health < 0)
             Destroy(gameObject);
+        GetComponent<SpriteRenderer>().color = baseColor * (0.5f+0.5f*(health / maxHealth));
     }
     public bool IsDamaged()
     {
